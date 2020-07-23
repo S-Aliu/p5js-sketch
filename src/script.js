818960d1250bba25bdf6d6f929828f055b064392
S-Aliu <40872039+S-Aliu@users.noreply.github.com>
2020-07-23 12:37:24 -0700

    feat: disable userSelect when drawing

--- src/script.js
+++ src/script.js
@@ -3,22 +3,20 @@ console.log('sketch file loaded')
 const s = (sketch) => {
 
    sketch.setup = () => {
-    console.log('sketch started')
-    let h = document.body.clientHeight;
-    let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-    c.position(0, 0);
-    c.style('pointer-events', 'none');
-    sketch.clear();
-  };
+     document.body.style['userSelect'] = 'none';
+     let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+     c.position(0, 0);
+     c.style('pointer-events', 'none');
+     sketch.clear();
+   };
 
    sketch.draw = () => {
-     console.log('sketch looping')
-    sketch.stroke(0);
-    sketch.strokeWeight(4);
-    if (sketch.mouseIsPressed) {
-      sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
-    }
-  };
-};
+     sketch.stroke(0);
+     sketch.strokeWeight(4);
+     if (sketch.mouseIsPressed) {
+       sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+     }
+   };
+ };
 
 let myp5 = new p5(s);
