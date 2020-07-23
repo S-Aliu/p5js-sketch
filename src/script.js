167218f4623efc888a7ec6002bfbee143cf2d9d3
S-Aliu <40872039+S-Aliu@users.noreply.github.com>
2020-07-23 13:00:52 -0700

    feat: implement rainbow colors

--- src/script.js
+++ src/script.js
@@ -2,16 +2,20 @@ console.log('sketch file loaded')
 
 const s = (sketch) => {
 
+   let brushHue = 0;
+
    sketch.setup = () => {
      document.body.style['userSelect'] = 'none';
      let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+     sketch.colorMode(sketch.HSB, 360, 100, 100);
      c.position(0, 0);
      c.style('pointer-events', 'none');
      sketch.clear();
    };
 
    sketch.draw = () => {
-     sketch.stroke(0);
+     brushHue = (brushHue + 1) % 360;
+     sketch.stroke(brushHue, 50, 80);
      sketch.strokeWeight(4);
      if (sketch.mouseIsPressed) {
        sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
